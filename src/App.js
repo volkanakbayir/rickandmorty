708ecd5648e1f96a6537d309ae681adb74ae9a28
volkanakbayir <wqlky@hotmail.com>
2019-07-03 16:51:31 +0300

    basic requirements available

--- src/App.js
+++ src/App.js
@@ -1,35 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route } from "react-router-dom";
 import './App.css';
+import CharacterList from './components/CharacterList/CharacterList';
+import CharacterDetail from './components/CharacterDetail/CharacterDetail';
 
-class App extends React.Component {
-
-  test() {
-    console.log(process.env.NODE_ENV)
-    console.log(process.env.API_URL);
-  }
-
-  render() {
-    this.test();
-    return (
-      <div className="App" >
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <React.Fragment>
+      <Route exact path="/" component={CharacterList} />
+      <Route exact path="/detail/:id" component={CharacterDetail} />
+    </React.Fragment>
+  )
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import * as serviceWorker from './serviceWorker';
+import store from './store'
+import saga from './sagas'
+import { sagaMiddleware } from './middleware'
+sagaMiddleware.run(saga);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>,
+    document.getElementById('root')
+);
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
